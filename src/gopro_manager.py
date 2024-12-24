d56413a0a7b34d5d0ba2956ca7ddc2eabd79db2b
GeorgeMichailov10 <george@michailov.org>
2024-12-23 21:11:16 -0800

    GoProManager work.

--- src/gopro_manager.py
+++ src/gopro_manager.py
@@ -1,9 +1,32 @@
 from open_gopro import WirelessGoPro
 import numpy as np
+import cv2
 import asyncio
+import threading
+import time
+from typing import List, Optional
+import os
+import dotenv
+
+dotenv.load_dotenv()
 
 class GoProManager:
     def __init__(self, ip_addresses):
         self.ip_addresses = ip_addresses
-        self.cameras = [WirelessGoPro(ip_address) for ip_address in self.ip_addresses]
+        self.cameras = [cv2.VideoCapture(0)]
         self.active_camera_index = 0
+        self.last_frame_buffers = [None]
+        self.active_stream_frame_buffer = None
+
+
+    def start_streams(self):
+        pass
+
+    def stop_streams(self):
+        pass
+
+    def switch_camera(self, index: int):
+        pass
+
+    
+        
